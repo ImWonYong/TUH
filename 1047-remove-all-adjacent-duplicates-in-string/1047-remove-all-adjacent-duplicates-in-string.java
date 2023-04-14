69d1eeaf636b90dec7a08822f1ecef112f857667
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2023-04-14 14:34:48 +0900

    Time: 80 ms (52.73%), Space: 43 MB (72.73%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            } 
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        
+        sb.reverse();
+        return sb.toString();
+    }
+}
