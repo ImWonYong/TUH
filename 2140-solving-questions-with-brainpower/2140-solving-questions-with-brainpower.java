4c192a4950bbb96cb4de27d67cf4e28c1263146b
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2023-04-19 16:11:36 +0900

    Time: 6 ms (78.81%), Space: 94.3 MB (71.52%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,26 +1,14 @@
 class Solution {
-    Map<Integer, Long> memo = new HashMap<>();
-    public long mostPoints(int[][] questions) {
-        return dp(0, questions);
-    }
-    
-    public long dp(int i, int[][] questions) {
-        if (i >= questions.length) {
-            return 0;
-        }
-        
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }
-        
-        int j = i + questions[i][1] + 1;
-        memo.put(i, Math.max(questions[i][0] + dp(j, questions), dp(i + 1, questions)));
-        
-        return memo.get(i);
-    }
-}
+	public long mostPoints(int[][] questions) {
+		int n = questions.length;
+		long[] dp = new long[n + 1];
 
-/*
-    dp(0) = (questions[0][0] + dp(0 + queustions[0][1] + 1), dp(1));
-    dp[1] = 
-*/
+		for (int i = n - 1; i >= 0; i--) {
+			int j = i + questions[i][1] + 1;
+
+			dp[i] = Math.max(questions[i][0] + dp[Math.min(j, n)], dp[i + 1]);
+		}
+
+		return dp[0];
+	}
+}
