c699bf85223d319391282d32e0ac20884fa952ce
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2023-04-23 19:45:21 +0900

    Time: 324 ms (37.93%), Space: 44.8 MB (65.52%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.java
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -3,39 +3,28 @@ class Solution {
     public int numberOfArrays(String s, int k) {
         int m = s.length();
         int[] dp = new int[m + 1];
-        return dfs(dp, 0, s, k);
-    }
-    
-    public int dfs(int[] dp, int start, String s, int k) {
-        if (dp[start] != 0) {
-            return dp[start];
-        }
+        dp[0] = 1;
         
-        if (start == s.length()) {
-            return 1;
-        }
-        
-        if (s.charAt(start) == '0') {
-            return 0;
-        }
-        
-        int count = 0;
-        for (int end = start; end < s.length(); end++) {
-            String currNum = s.substring(start, end + 1);
-            if (Long.parseLong(currNum) > k) {
-                break;
+        for (int start = 0; start < m; start++) {
+            if (s.charAt(start) == '0') continue;
+            
+            for (int end = start; end < m; end++) {
+                String currNum = s.substring(start, end + 1);
+                
+                if (Long.parseLong(currNum) > k) break;
+                
+                dp[end + 1] = (dp[end + 1] + dp[start]) % mod;
             }
-            count = (count + dfs(dp, end + 1, s, k)) % mod;
         }
-        dp[start] = count;
-        return dp[start];
+        
+        return dp[m];
     }
 }
 
 /*
     Integer 배열을 프린트하는 프로그램
     배열의 숫자들이 [1, k] 사이의 숫자여야 한다.
-    0으로 시작하는 것은 세지 않는다.
     
+    0으로 시작하는 것은 세지 않는다.
     dp[start] 는 해당 숫자부터 만들 수 있는 숫자 배열 갯수
 */
