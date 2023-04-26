e30658d2194eb91b812c36448bd6b8929607319f
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2023-04-26 12:30:17 +0900

    Time: 1 ms (67.11%), Space: 42.1 MB (73.96%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,23 +1,24 @@
 class Solution {
-	Map<Integer, Integer> memo = new HashMap<>();
-	
+    // 무엇인가의 최대 최소, 이전의 결정에 다음 결정이 의존되어 있다 -> DP
 	public int minCostClimbingStairs(int[] cost) {
-		return dp(cost.length, cost);
+		int minimumCost[] = new int[cost.length + 1];
+        
+        for (int i = 2; i < minimumCost.length; i++) {
+            int oneStep = minimumCost[i - 1] + cost[i - 1];
+            int twoStep = minimumCost[i - 2] + cost[i - 2];
+            minimumCost[i] = Math.min(oneStep, twoStep);
+        }
+        
+        return minimumCost[minimumCost.length - 1];
 	}
-
-	public int dp(int i, int[] cost) {
-		if (i == 0 || i == 1) {
-			return 0;
-		}
-
-		if (memo.containsKey(i)) {
-			return memo.get(i);
-		}
-
-		int min = Math.min(dp(i - 1, cost) + cost[i - 1], dp(i - 2, cost) + cost[i - 2]);
-		
-		memo.put(i, min);
-
-		return min;
-	}
+    
 }
+
+/*
+    3가지를 찾아야 합니다.
+    1. 재귀 함수의 리턴과 입력값
+    2. 재귀식
+    3. base case
+    
+    dfs(2) = cost[i] + min(dfs(1) , dfs(0));
+*/
